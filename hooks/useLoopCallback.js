c1426971d0e850c076291f36d3a3c61d353b7377
Tonix-Tuft <antonytuft@gmail.com>
2020-02-24 09:56:51 +0100

    Changed @param doccomment

--- hooks/useLoopCallback.js
+++ hooks/useLoopCallback.js
@@ -29,7 +29,7 @@ import ImmutableLinkedOrderedMap from "immutable-linked-ordered-map";
 /**
  * Hook genereting a callback to easily loop through a data structure.
  *
- * @param {*} dataStructure A data structure.
+ * @param {Array|ImmutableLinkedOrderedMap} dataStructure A data structure.
  * @return {(fn: (value: *) => *)) => Array} A function which receives a callback to loop through
  *                                           the values of the data structure returning an array
  *                                           with the mapped values.
