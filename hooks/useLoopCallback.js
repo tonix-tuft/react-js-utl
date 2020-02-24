aea5908b2aaf2a231dcf961198319b22cd32ca26
Tonix-Tuft <antonytuft@gmail.com>
2020-02-24 09:56:16 +0100

    Added missing doccomments

--- hooks/useLoopCallback.js
+++ hooks/useLoopCallback.js
@@ -26,6 +26,14 @@
 import useFactory from "./useFactory";
 import ImmutableLinkedOrderedMap from "immutable-linked-ordered-map";
 
+/**
+ * Hook genereting a callback to easily loop through a data structure.
+ *
+ * @param {*} dataStructure A data structure.
+ * @return {(fn: (value: *) => *)) => Array} A function which receives a callback to loop through
+ *                                           the values of the data structure returning an array
+ *                                           with the mapped values.
+ */
 export default function useLoopCallback(dataStructure) {
   const loopCallback = useFactory(
     () => [
