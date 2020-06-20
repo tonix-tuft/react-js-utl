a9c1974c392a4c34a359229350558b0e2a3aceb2
Anton Bagdatyev (Tonix) <antonytuft@gmail.com>
2020-06-20 11:17:55 +0200

    Added useClientRect hook (missing file)

---
+++ src/hooks/useClientRect.js
@@ -0,0 +1,44 @@
+/*
+ * Copyright (c) 2020 Anton Bagdatyev (Tonix)
+ *
+ * Permission is hereby granted, free of charge, to any person
+ * obtaining a copy of this software and associated documentation
+ * files (the "Software"), to deal in the Software without
+ * restriction, including without limitation the rights to use,
+ * copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following
+ * conditions:
+ *
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+ * OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+ * HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+ * WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+ * OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+import { useState, useCallback } from "react";
+
+/**
+ * Hook returning a ref callback to measure the dimensions of a DOM element and the current dimensions of the measured element.
+ *
+ * @see https://reactjs.org/docs/hooks-faq.html#how-can-i-measure-a-dom-node
+ *
+ * @return {Array} An array containing the ref callback at index 0 and the dimensions (an object with a "width" and "height"
+ *                 property) of the last measurement of the element at index 1.
+ */
+export default function useClientRect() {
+  const [rect, setRect] = useState(null);
+  const ref = useCallback(node => {
+    if (node !== null) {
+      setRect(node.getBoundingClientRect());
+    }
+  }, []);
+  return [ref, rect];
+}
