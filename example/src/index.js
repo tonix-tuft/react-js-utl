246341e4b0b32179e2d1baee9981f5081ba56b5a
Anton Bagdatyev (Tonix) <antonytuft@gmail.com>
2022-05-18 22:21:51 +0200

    Upgraded to React 18

--- example/src/index.js
+++ example/src/index.js
@@ -1,7 +1,9 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
+import React from "react";
+import { createRoot } from "react-dom/client";
 
-import './index.css'
-import App from './App'
+import "./index.css";
+import App from "./App";
 
-ReactDOM.render(<App />, document.getElementById('root'))
+const container = document.getElementById("root");
+const root = createRoot(container); // createRoot(container!) if you use TypeScript
+root.render(<App />);
